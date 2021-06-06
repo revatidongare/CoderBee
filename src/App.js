52204591883c5be2f91567c894f5dd67eb80ad35
revatidongare <revatid15@gmail.com>
2021-06-06 20:47:38 +0530

    about section

--- src/App.js
+++ src/App.js
@@ -1,18 +1,38 @@
-import React from 'react'
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import MyNavbar from "./components/my-navbar/mynavbar.component"
-import MyCarousal from "./components/my-carousal/my-carousal.component"
-import TitleMessage from './components/title-message/title-message.component'
+import { Parallax } from 'react-parallax';
+import MyNavbar from "./components/my-navbar/mynavbar.component";
+import MyCarousal from "./components/my-carousal/my-carousal.component";
+import TitleMessage from "./components/title-message/title-message.component";
+import About from "./pages/about/about.component";
+import Container from "react-bootstrap/Container";
+import Fade from "react-reveal/Fade";
 
-const App = () =>{
+const App = () => {
   return (
-    <div>
+    <div className="App" style={{ position: "relative" }}>
       <MyNavbar/>
       <MyCarousal/>
       <TitleMessage/>
+      <div>
+      <Parallax
+        blur={{ min: -15, max: 15 }}
+        bgImage={require('./assets/img/parallex/background.png')}
+        bgImageAlt=""
+        strength={-200}
+    >  
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+              <About/>
+              </Fade>
+            </Container>
+              
+        </Parallax>
+      
+      </div>      
     </div>
   );
-}
+};
 
 export default App;
