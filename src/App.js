ec777d40971865f6a1ffe7cf7abce0b50081ed23
revatidongare <revatid15@gmail.com>
2021-06-10 20:38:08 +0530

    experiemce tab done

--- src/App.js
+++ src/App.js
@@ -9,7 +9,9 @@ import TitleMessage from "./components/title-message/title-message.component";
 import Skills from "./pages/skills/skills.components";
 import About from "./pages/about/about.component";
 import Container from "react-bootstrap/Container";
+import Slide from 'react-reveal/Slide';
 import Fade from "react-reveal/Fade";
+import Experience from "./pages/experience/experience.compnent";
 
 const App = () => {
   return (
@@ -33,15 +35,26 @@ const App = () => {
         </Parallax>
       
       </div>    
+      {/* skills */}
+      <div>
+            <Container className="container-box rounded">
+              <Slide bottom duration={500}>
+                <hr />
+              <Skills/> 
+              </Slide>
+            </Container>
+            
+      </div> 
+      {/* experience */}
       <div>
             <Container className="container-box rounded">
               <Fade duration={500}>
-              <Skills/> 
+                <hr />
+              <Experience/> 
               </Fade>
             </Container>
-              
             
-      </div> 
+      </div>
        
     </div>
   );
