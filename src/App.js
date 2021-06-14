b3f3c053b5a5de21f8a24d6b38b6d90038c4f290
revatidongare <revatid15@gmail.com>
2021-06-14 23:40:21 +0530

    contact and footer along with projects

--- src/App.js
+++ src/App.js
@@ -12,11 +12,21 @@ import Container from "react-bootstrap/Container";
 import Slide from 'react-reveal/Slide';
 import Fade from "react-reveal/Fade";
 import Experience from "./pages/experience/experience.compnent";
+import Contact from "./pages/contact-form/contact-form.component";
+import Footer from "./components/footer/footer.component";
+import ProjectTimeline from './components/projects-timeline/projects-timeline.component';
+import Particles from 'react-particles-js';
+import { particlesOptions } from "./particlesOptions";
 
 const App = () => {
   return (
     <div className="App" style={{ position: "relative" }}>
       <MyNavbar/>
+      <Particles
+        className="particles particles-box"
+        params={particlesOptions}
+      />
+      
       <MyCarousal/>
       <TitleMessage/>
       <div>
@@ -55,7 +65,30 @@ const App = () => {
             </Container>
             
       </div>
-       
+
+      {/* Proj */}
+      <div>
+            <Container className="container-box rounded">
+              <Slide bottom duration={500}>
+                <hr />
+              <ProjectTimeline/> 
+              </Slide>
+            </Container>
+            
+      </div> 
+       {/* contact */}
+      <div>
+            <Container className="container-box rounded">
+              <Fade duration={500}>
+                <hr />
+              <Contact/> 
+              </Fade>
+            </Container>
+            
+      </div>
+
+      <hr />
+      <Footer/>
     </div>
   );
 };
